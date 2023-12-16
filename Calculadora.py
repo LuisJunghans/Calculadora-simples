58d8d76714f028094a2077015b0968217efad1cb
luisf222 <felipejunghans@gmail.com>
2023-12-16 03:32:20 -0300

    Primeiro Commit

---
+++ Calculadora.py
@@ -0,0 +1,16 @@
+print('==========CALCULADORA==========')
+while True:
+    num1 = float(input('Digite o primeiro numero: '))
+    operacao = input('Qual operação deseja realizar? (*, /, +, -)')
+    num2 = float(input('Digite o segundo numero: '))
+    if operacao == '*':
+        print(num1 * num2)
+    elif operacao == '/':
+        print(num1 / num2)
+    elif operacao == '-':
+        print(num1 - num2)
+    elif operacao == '+':
+        print(num1 + num2)
+    else:
+        print('Entrada inválida. Por favor digite uma das opções acima.')
+    break
